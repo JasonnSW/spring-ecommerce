a07dd83ae5f8b513297d4ad40fc1b820aa5ef46b
JasonnSW <jasonsurya17@gmail.com>
2025-05-03 16:09:41 +0700

    feat(auth): Add user mapping methods for registration and updates

--- src/main/java/com/spring/store/mappers/UserMapper.java
+++ src/main/java/com/spring/store/mappers/UserMapper.java
@@ -1,12 +1,19 @@
 package com.spring.store.mappers;
 
+import com.spring.store.dto.RegisterUserRequest;
+import com.spring.store.dto.UpdateUserRequest;
 import org.mapstruct.Mapper;
 import com.spring.store.entities.User;
 import com.spring.store.dto.UserDto;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
+
+    User toEntity(RegisterUserRequest request);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
